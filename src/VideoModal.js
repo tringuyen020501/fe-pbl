805b26c9c8ea33e97508cdf5d365e8e3c44964e4
Nguyen Tran Minh Tri <tmtn@Nguyens-Laptop.local>
2024-06-04 10:09:33 +0700

    load

--- src/VideoModal.js
+++ src/VideoModal.js
@@ -20,6 +20,7 @@ const VideoModal = ({ isOpen, onRequestClose, videoUrl }) => {
           width="100%"
           height="400"
           src={videoUrl.replace("watch?v=", "embed/")}
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
