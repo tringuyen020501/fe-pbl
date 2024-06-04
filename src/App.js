780fd3e72aec336e1f03153e34c4290f7f1932ff
Nguyen Tran Minh Tri <tmtn@Nguyens-Laptop.local>
2024-06-04 09:18:56 +0700

    add map key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import "./App.css";
+
+const containerStyle = {
+  width: "100%",
+  height: "600px",
+};
+
+const center = {
+  lat: 21.0285,
+  lng: 105.8542,
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WebGIS Đơn Giản với React và Google Maps</h1>
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+          <Marker position={center} />
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 }
